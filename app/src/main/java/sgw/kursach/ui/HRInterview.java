0fbef8ad99621a6665a4e4fb9227713fab5622c9
Kate <kate.severinko.work@gmail.com>
2018-05-18 12:23:13 +0300

    kk

--- app/src/main/java/sgw/kursach/ui/HRInterview.java
+++ app/src/main/java/sgw/kursach/ui/HRInterview.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,8 +16,8 @@ public class HRInterview extends Activity implements View.OnClickListener {
     @BindView(R.id.seekBarExpectation)
     SeekBar seekBarExpectation;
 
-    @BindView(R.id.seekBarLoyalty)
-    SeekBar seekBarLoyalty;
+    @BindView(R.id.seekBarInitiative)
+    SeekBar seekBarInitiative;
 
     @BindView(R.id.seekBarMotivation)
     SeekBar seekBarMotivation;
@@ -27,12 +28,144 @@ public class HRInterview extends Activity implements View.OnClickListener {
     @BindView(R.id.enterInfo)
     Button enterInfo;
 
+    @BindView(R.id.seekResultExpectation)
+    TextView seekResultExpectation;
+
+    @BindView(R.id.seekResultInitiative)
+    TextView seekResultInitiative;
+
+    @BindView(R.id.seekResultMotivation)
+    TextView seekResultMotivation;
+
+    @BindView(R.id.seekResultFlexibility)
+    TextView seekResultFlexibility;
+
+    @BindView(R.id.seekResultResponsibility)
+    TextView seekResultResponsibility;
+
+    @BindView(R.id.seekBarResponsibility)
+    SeekBar seekBarResponsibility;
+
+    @BindView(R.id.seekResultFrustration)
+    TextView seekResultFrustration;
+
+    @BindView(R.id.seekBarFrustration)
+    SeekBar seekBarFrustration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hr_layout);
         ButterKnife.bind(this);
+
+        enterInfo.setOnClickListener(this);
+
+        initializeSeekBars();
+
+    }
+
+    private void initializeSeekBars() {
+
+        seekBarExpectation.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekResultExpectation.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBarInitiative.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekResultInitiative.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBarMotivation.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekResultMotivation.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBarFlexibility.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekResultFlexibility.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBarResponsibility.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekResultResponsibility.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBarFrustration.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekResultFrustration.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
